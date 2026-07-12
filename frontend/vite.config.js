a51b6d287d26519c77de17ffa8a2bde7c7c0e36e
bala-murali-battu <balamuralib2302@gmail.com>
2026-07-12 07:53:07 +0530

    Fix Vite plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // or '@vitejs/plugin-react' depending on what is on line 2
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TravelRoom-App/', // Must match your exact repository name from the image!
+  base: '/TravelRoom-App/',
 })
